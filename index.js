6d1ba1223568a12ddab4a0d499a8493663c94dd3
tricyvic <muhitavictor69@gmail.com>
2024-04-17 16:58:24 +0300

    display input Text

---
+++ index.js
@@ -0,0 +1,18 @@
+// Guess either 0 or 1
+// console.log(Math.round(Math.random()));
+
+const inputBox = document.querySelector(".input-box");
+const listContainer = document.querySelector(".list-container");
+const addText = document.querySelector(".addText")
+
+addText.addEventListener("click",()=>{
+    if (inputBox.value === "") {
+        alert("Please input Something");
+    } else {
+        let input = document.createElement('li');
+        input.innerHTML = inputBox.value;
+        listContainer.appendChild(input);
+        inputBox.value = "";
+    }
+})
+
